342bf115c4b3ad65e2fce8ff55d3cce9c83c3796
shivasrk12345 <jagdish.saini@teamcomputers.com>
2021-02-12 14:52:10 +0530

    deploy on heroku

--- store_assesment/settings.py
+++ store_assesment/settings.py
@@ -138,4 +138,4 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
